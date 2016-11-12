17bf98603e93df425991a2336284c8f1a3b7234d
mickey0142 <59070142@it.kmitl.ac.th>
2016-11-12 15:55:10 +0700

    fix error in alldata.txt and fix bug that happen from too long state name

--- guncrime.py
+++ guncrime.py
@@ -8,6 +8,9 @@ def collect_data():
         if len(i) > 5:
             i[2] = i[2] + i[3]
             i.remove(i[3])
+        if len(i) > 5:
+            i[2] = i[2] + i[3]
+            i.remove(i[3])
         if not i[2] in crime[i[1]]:
             crime[i[1]][i[2]] = []
             crime[i[1]][i[2]].append([i[0], i[3], i[4]])
@@ -16,4 +19,21 @@ def collect_data():
     return crime
 # data is in {'year':{'state':[month, kill, injured]}}
 
-collect_data()
+def numberofcrime():
+    """get data that what state have the highest number of crime and that number"""
+    data = collect_data()
+    number = {}
+    for i in data:
+        for j in data[i]:
+            for k in data[i][j]:
+                print(k)
+                if not k[0] in number:
+                    number[k[0]] = 1
+                else:
+                    number[k[0]] += 1
+    number["all_year"] = 0
+    for i in number:
+        if i.isdigit():
+            number["all_year"] += number[i]
+
+numberofcrime()
