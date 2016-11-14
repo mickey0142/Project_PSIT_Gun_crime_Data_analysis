e84a216ae3f24c4fdbffa571ea944ab15c6fb0a2
mickey0142 <59070142@it.kmitl.ac.th>
2016-11-14 16:44:46 +0700

    add new function

--- guncrime.py
+++ guncrime.py
@@ -1,4 +1,5 @@
 """US Guncrime data analysis"""
+import pygal
 def collect_data():
     """get data from file .txt into variable 'crime'"""
     file = open('alldata.txt')
@@ -23,7 +24,7 @@ def numberofcrime():
     """get data that what state have the highest number of crime and that number"""
     data = collect_data()# move collect_data() into main function and call it from there then just put that variable into this function
     number = {}
-    for i in data:# change data into list of year that you want to use
+    for i in data:# change data into list of year from input change this after main function
         number[i] = {}
         for j in data[i]:
             number[i][j] = {}
@@ -49,7 +50,21 @@ def numberofcrime():
                 if j.isdigit():
                     all_year['sum_year'] += all_year[i][j]
                     all_year[i]['one_year'] += all_year[i][j]
-    return number#data contain how many crime happen each state and each month
-    return all_year# data contain how many crime happen each month each year and all
+    return number, all_year
+    #number -> data contain how many crime happen each state and each month
+    #all_year -> data contain how many crime happen each month each year and all
+# def sort(): sort function
 
-numberofcrime()
+def graph():
+    """use data to plot graph"""
+    number, all_year = numberofcrime()
+    use = list(all_year['2013'].values())
+    use2 = list(all_year['2014'].values())
+    line_chart = pygal.Line()
+    line_chart.title = 'guncrime'
+    line_chart.x_labels = map(str, range(1, 13))
+    line_chart.add('2013', use)
+    line_chart.add('2014', use2)
+    line_chart.render_to_file('test.svg')
+
+graph()
